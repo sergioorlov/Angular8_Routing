a5ff4843c75fa181b7a3a31bd0539f68abc2dc00
Sergio Orlov <sergioorlove@gmail.com>
2019-08-27 18:11:00 +0300

    Add some components!

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {
+
+  constructor(private postsService: PostsService) {}
+
+}
